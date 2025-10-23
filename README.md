26ebbc6606ec426534cacfb503dc8fb5f8934048
sunheeday <k2mseungh2@gmail.com>
2025-10-23 16:02:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zkmpc-api
+
---
+++ src/main/java/com/zkrypto/zkmpc_api/ZkmpcApiApplication.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkmpc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkmpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkmpcApiApplication.class, args);
+	}
+
+}
