1e6c2b7b8775b64d13727a43a5a209981f3739f6
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:31:28 +0900

    Update ZkmpcApiApplication.java

--- src/main/java/com/zkrypto/zkmpc_api/ZkmpcApiApplication.java
+++ src/main/java/com/zkrypto/zkmpc_api/ZkmpcApiApplication.java
@@ -2,8 +2,10 @@ package com.zkrypto.zkmpc_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.zkrypto.zkmpc_api.domain")
 public class ZkmpcApiApplication {
 
 	public static void main(String[] args) {
