72df2fd5ac3b1b4af0a64744b7cb50a86705de5f
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:30:11 +0900

    Create JpaEnterpriseRepository.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaEnterpriseRepository.java
@@ -0,0 +1,10 @@
+package com.zkrypto.zkmpc_api.infrastructure.persistence;
+
+import com.zkrypto.zkmpc_api.domain.enterprise.domain.entity.Enterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaEnterpriseRepository extends JpaRepository<Enterprise, String> { // PK 타입이 String (enterpriseId)
+    Optional<Enterprise> findByEnterpriseId(String enterpriseId);
+}
