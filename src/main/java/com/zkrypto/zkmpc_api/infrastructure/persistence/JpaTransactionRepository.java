f85dec37e10f0360882b2142ae31403913e0a8b3
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:31:17 +0900

    Create JpaTransactionRepository.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaTransactionRepository.java
@@ -0,0 +1,10 @@
+package com.zkrypto.zkmpc_api.infrastructure.persistence;
+
+import com.zkrypto.zkmpc_api.domain.transaction.domain.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaTransactionRepository extends JpaRepository<Transaction, Long> { // PK 타입은 Long (id)
+    Optional<Transaction> findByTransactionId(String transactionId);
+}
