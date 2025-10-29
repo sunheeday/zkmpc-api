84975dac923c47fcb5ab7126d2aada26c9ad3fef
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:31:09 +0900

    Create JpaGroupRepository.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaGroupRepository.java
@@ -0,0 +1,10 @@
+package com.zkrypto.zkmpc_api.infrastructure.persistence;
+
+import com.zkrypto.zkmpc_api.domain.group.domain.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaGroupRepository extends JpaRepository<Group, Long> {
+    Optional<Group> findByGroupId(String groupId);
+}
