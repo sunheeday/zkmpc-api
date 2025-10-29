781c7c4b55d607516a18003627bbaf00d7c34421
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:27:52 +0900

    Create Group.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/domain/group/domain/entity/Group.java
@@ -0,0 +1,57 @@
+package com.zkrypto.zkmpc_api.domain.group.domain.entity;
+
+import com.zkrypto.zkmpc_api.domain.enterprise.domain.entity.Enterprise;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 내부 PK
+
+    @Column(name = "group_id", unique = true, nullable = false)
+    private String groupId; // 외부용 ID (ERD의 groupID)
+
+    @Column(name = "threshold", nullable = false)
+    private Integer threshold; // 출금 승인 최소 인원
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "group_member", joinColumns = @JoinColumn(name = "group_id"))
+    private List<String> memberIds;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "group_enterprise",
+            joinColumns = @JoinColumn(name = "group_id")
+    )
+    @Column(name = "enterprise_id")
+    private Set<String> enterpriseIds = new HashSet<>();
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public Group(String groupId, List<String> memberIds, Set<String> enterpriseIds, Integer threshold) {
+        // 도메인 규칙 검증: enterprise는 최소 2개 이상이어야 함
+        if (enterpriseIds == null || enterpriseIds.size() < 2) {
+            throw new IllegalArgumentException("그룹 등록을 위해서는 최소 2개 이상의 엔터프라이즈 ID가 필요합니다.");
+        }
+
+        this.groupId = groupId;
+        this.memberIds = memberIds;
+        this.enterpriseIds = enterpriseIds;
+        this.threshold = threshold;
+        this.createdAt = LocalDateTime.now();
+    }
+}
