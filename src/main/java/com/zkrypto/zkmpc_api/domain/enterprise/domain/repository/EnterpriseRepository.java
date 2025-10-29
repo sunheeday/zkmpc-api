138c9f9a6e059dfd7eb7cf4bce02e9c39ddb01fd
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:26:55 +0900

    Create EnterpriseRepository.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/domain/enterprise/domain/repository/EnterpriseRepository.java
@@ -0,0 +1,15 @@
+package com.zkrypto.zkmpc_api.domain.enterprise.domain.repository;
+
+import com.zkrypto.zkmpc_api.domain.enterprise.domain.entity.Enterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EnterpriseRepository{
+    Enterprise save(Enterprise enterprise);
+    Optional<Enterprise> findByEnterpriseId(String enterpriseId);
+    List<Enterprise> findAll();
+}
