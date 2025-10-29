04154188be36082ef07fd1ec06ecbce0333a5edd
sunheeday <k2mseungh2@gmail.com>
2025-10-29 16:16:10 +0900

    feat: groupEnterprise entity 생성

---
+++ src/main/java/com/zkrypto/zkmpc_api/domain/group/domain/entity/GroupEnterprise.java
@@ -0,0 +1,35 @@
+package com.zkrypto.zkmpc_api.domain.group.domain.entity;
+
+import com.zkrypto.zkmpc_api.domain.enterprise.domain.entity.Enterprise;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="group_enterprise")
+public class GroupEnterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "enterprise_id", nullable = false, length = 64)
+    private Enterprise enterprise;
+
+    public GroupEnterprise(Group group, Enterprise enterprise) {
+        this.group = group;
+        this.enterprise = enterprise;
+    }
+
+    public String getEnterpriseId(){
+        return this.enterprise.getEnterpriseId();
+    }
+
+}
