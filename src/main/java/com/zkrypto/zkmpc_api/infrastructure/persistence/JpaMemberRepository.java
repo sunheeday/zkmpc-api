ea03a2f6e9b998c7cbd2264d8c6265fe091a8054
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:31:13 +0900

    Create JpaMemberRepository.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaMemberRepository.java
@@ -0,0 +1,11 @@
+package com.zkrypto.zkmpc_api.infrastructure.persistence;
+
+import com.zkrypto.zkmpc_api.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaMemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByMemberId(String memberId);
+    Optional<Member> findByAddress(String address);
+}
