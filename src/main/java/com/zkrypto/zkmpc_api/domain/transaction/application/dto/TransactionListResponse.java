214c4e32aef73ce215f61c5efcb3d2e75bb6bd61
sunheeday <k2mseungh2@gmail.com>
2025-10-29 14:28:10 +0900

    Create TransactionListResponse.java

---
+++ src/main/java/com/zkrypto/zkmpc_api/domain/transaction/application/dto/TransactionListResponse.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkmpc_api.domain.transaction.application.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+public class TransactionListResponse {
+    private final List<TransactionResponse> transactions;
+
+    public TransactionListResponse(List<TransactionResponse> transactions) {
+        this.transactions = transactions;
+    }
+}
