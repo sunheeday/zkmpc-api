62106dd86dfeeeb6170f96d0be7e01bc33305593
sunheeday <k2mseungh2@gmail.com>
2025-11-02 16:39:47 +0900

    fix: findByGroup_GroupId 및 findByEmail 추가

--- src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaMemberRepository.java
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/persistence/JpaMemberRepository.java
@@ -1,6 +1,6 @@
 package com.zkrypto.zkmpc_api.infrastructure.persistence;
 
-import com.zkrypto.zkmpc_api.domain.member.entity.Member;
+import com.zkrypto.zkmpc_api.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface JpaMemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByMemberId(String memberId);
     Optional<Member> findByAddress(String address);
+    Optional<Member> findByGroup_GroupId(String groupId);
+    Optional<Member> findByEmail(String email);
 }
