b37cd05d508813b85163600496355435278f9e69
sunheeday <k2mseungh2@gmail.com>
2025-11-02 16:56:28 +0900

    fix: zkMpcClient 주입 및 zkMPC SIGNING 프로토콜  zkMpcClient로 시작

--- src/main/java/com/zkrypto/zkmpc_api/domain/transaction/application/service/TransactionService.java
+++ src/main/java/com/zkrypto/zkmpc_api/domain/transaction/application/service/TransactionService.java
@@ -10,29 +10,44 @@ import com.zkrypto.zkmpc_api.domain.transaction.application.dto.TransactionStatu
 import com.zkrypto.zkmpc_api.domain.transaction.domain.constant.TransactionStatus;
 import com.zkrypto.zkmpc_api.domain.transaction.domain.entity.Transaction;
 import com.zkrypto.zkmpc_api.domain.transaction.domain.repository.TransactionRepository;
+import com.zkrypto.zkmpc_api.infrastructure.ZkMpcClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.List;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.Sign;
+import org.web3j.crypto.TransactionEncoder;
+import org.web3j.utils.Convert;
 
 @Service
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
     private final GroupService groupService;
+    private final ZkMpcClient zkMpcClient;
 
-    public TransactionService(TransactionRepository transactionRepository, GroupService groupService) {
+    @Value("${ethereum.chain-id}")
+    private Long chainId;
+
+    public TransactionService(TransactionRepository transactionRepository, GroupService groupService, ZkMpcClient zkMpcClient) {
         this.transactionRepository = transactionRepository;
         this.groupService = groupService;
+        this.zkMpcClient = zkMpcClient;
+
     }
 
     // 1. 거래 요청 및 SIGNING 프로토콜 시작 (POST /v1/transaction)
     @Transactional
     public TransactionResponse requestTransaction(TransactionRequest request) {
         String newTransactionId = UUID.randomUUID().toString();
-        String groupId = request.getGroupId();
 
-        Group group = groupService.getGroupById(groupId);
+        Group group = groupService.getGroupByAddress(request.getFrom());
+        String groupId = group.getGroupId();
 
         // 1.1. 거래 엔티티 생성 및 저장 (PENDING 상태)
         Transaction transaction = new Transaction(
@@ -44,11 +59,41 @@ public class TransactionService {
         );
         transactionRepository.save(transaction);
 
-        byte[] messageToSign = newTransactionId.getBytes();
+//        byte[] messageToSign = newTransactionId.getBytes();
+        //TODO 그..from to 값을 이용해서 이더리움 트랜잭션 형태로 만들기 && messageToSign을 유저한테 반환해야함
 
-        // 1.2. zkMPC SIGNING 프로토콜 시작
+        String senderAddress = transaction.getSender(); //from
+        String toAddress = transaction.getReceiver(); //to
+        BigInteger nonce = BigInteger.ZERO;
+        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L); // 20 Gwei
+        BigInteger gasLimit = BigInteger.valueOf(21_000L);
+
+        BigInteger valueInWei = Convert.toWei(
+                String.valueOf(request.getValue()),
+                Convert.Unit.ETHER
+        ).toBigInteger();
+
+        RawTransaction rawTransaction = RawTransaction
+                .createTransaction(nonce, gasPrice, gasLimit, toAddress, valueInWei, "");
+
+        byte[] encodedTxForSigning = TransactionEncoder.encode(
+                rawTransaction,
+                new Sign.SignatureData(BigInteger.valueOf(this.chainId).toByteArray(),
+                        new byte[]{}, new byte[]{}));
+
+        //사용자
+        String memberId = groupService.getMemberIdByGroupId(groupId);
+
+        //파티들 + 사용자
+        List<String> memberIds = new ArrayList<>(group.getEnterpriseIds());
+        memberIds.add(memberId);
+
+        Integer threshold = group.getThreshold();
+
+
+
         try {
-            groupService.startZkMpcProtocol("SIGNING", groupId, null, 0, messageToSign);
+            zkMpcClient.requestStart("SIGNING", groupId, memberIds, threshold, encodedTxForSigning);
         } catch (Exception e) {
             throw new RuntimeException("SIGNING 프로토콜 시작 실패", e);
         }
@@ -63,8 +108,6 @@ public class TransactionService {
                 .orElseThrow(() -> new IllegalArgumentException("거래를 찾을 수 없습니다: " + request.getTransactionId()));
 
         transaction.updateStatus(TransactionStatus.COMPLETED, request.getTxId(), request.getVat());
-
-        transactionRepository.save(transaction);
     }
 
     // 3. 단일 거래 조회 (GET /v1/transaction)
