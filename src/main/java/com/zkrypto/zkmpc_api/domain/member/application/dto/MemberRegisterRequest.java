e3f252a2025bb1e08d94b965ca556057e90d507b
sunheeday <k2mseungh2@gmail.com>
2025-11-13 16:03:39 +0900

    fix: setter제거

--- src/main/java/com/zkrypto/zkmpc_api/domain/member/application/dto/MemberRegisterRequest.java
+++ src/main/java/com/zkrypto/zkmpc_api/domain/member/application/dto/MemberRegisterRequest.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRegisterRequest {
 
     @NotBlank
