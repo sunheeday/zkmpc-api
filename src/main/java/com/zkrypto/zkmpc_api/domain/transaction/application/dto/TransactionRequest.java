24e1b76f9abb0f69caea8bae80609f99c52ce223
sunheeday <k2mseungh2@gmail.com>
2025-11-13 16:06:47 +0900

    fix: lombok constructor 추가

--- src/main/java/com/zkrypto/zkmpc_api/domain/transaction/application/dto/TransactionRequest.java
+++ src/main/java/com/zkrypto/zkmpc_api/domain/transaction/application/dto/TransactionRequest.java
@@ -2,11 +2,15 @@ package com.zkrypto.zkmpc_api.domain.transaction.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionRequest {
     @NotBlank
     private String from;
