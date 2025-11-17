2e8e9a7e08064dfb5a2d8825f282a3ebe2b704b5
sunheeday <k2mseungh2@gmail.com>
2025-11-17 23:39:20 +0900

    Update RedisAuthCodeManager.java

--- src/main/java/com/zkrypto/zkmpc_api/infrastructure/cache/RedisAuthCodeManager.java
+++ src/main/java/com/zkrypto/zkmpc_api/infrastructure/cache/RedisAuthCodeManager.java
@@ -33,7 +33,7 @@ public class RedisAuthCodeManager implements AuthCodeManager {
     public Optional<String> get(String email) {
         String key = createRedisKey(email);
         String code = redisTemplate.opsForValue().get(key);
-        return Optional.of(code);
+        return Optional.ofNullable(code);
     }
 
     @Override
