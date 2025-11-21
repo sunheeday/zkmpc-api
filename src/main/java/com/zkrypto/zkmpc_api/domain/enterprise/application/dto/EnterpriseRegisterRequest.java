361dbb49dca0a23293489e1a5ad72c9e07208443
sunheeday <k2mseungh2@gmail.com>
2025-11-21 23:43:21 +0900

    f

--- src/main/java/com/zkrypto/zkmpc_api/domain/enterprise/application/dto/EnterpriseRegisterRequest.java
+++ src/main/java/com/zkrypto/zkmpc_api/domain/enterprise/application/dto/EnterpriseRegisterRequest.java
@@ -2,9 +2,11 @@ package com.zkrypto.zkmpc_api.domain.enterprise.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
 @Setter
 public class EnterpriseRegisterRequest {
     @NotBlank
