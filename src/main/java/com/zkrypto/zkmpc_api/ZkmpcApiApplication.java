dfb655addc284ec0974e9b9b027ebeebd45db866
sunheeday <k2mseungh2@gmail.com>
2025-11-21 14:44:41 +0900

    fix: basePackage 경로 수정

--- src/main/java/com/zkrypto/zkmpc_api/ZkmpcApiApplication.java
+++ src/main/java/com/zkrypto/zkmpc_api/ZkmpcApiApplication.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.zkrypto.zkmpc_api.domain")
-
+@EnableJpaRepositories(basePackages = "com.zkrypto.zkmpc_api.infrastructure.persistence")
 public class ZkmpcApiApplication {
 
 	public static void main(String[] args) {
