c7786930f12740069fb00a0286edab7ae226bbf0
sunheeday <k2mseungh2@gmail.com>
2025-11-21 18:51:07 +0900

    fix: activeprofile 추가

--- src/test/java/com/zkrypto/zkmpc_api/ZkmpcApiApplicationTests.java
+++ src/test/java/com/zkrypto/zkmpc_api/ZkmpcApiApplicationTests.java
@@ -1,11 +1,13 @@
 package com.zkrypto.zkmpc_api;
 
+import com.zkrypto.zkmpc_api.infrastructure.ZkMpcClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZkmpcApiApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
