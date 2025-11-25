d1db4a628a5c64f8123ea72c9a6f1a628548a720
sunheeday <k2mseungh2@gmail.com>
2025-11-25 21:03:30 +0900

    delete: deprecated files

--- src/main/java/com/zkrypto/zkmpc_api/domain/mpc/application/dto/MpcStatusUpdate.java
+++
@@ -1,8 +0,0 @@
-package com.zkrypto.zkmpc_api.domain.mpc.application.dto;
-
-public record MpcStatusUpdate(
-        String mpcId,
-        String statusMessage,
-        int roundNum
-) {
-}
--- src/main/java/com/zkrypto/zkmpc_api/domain/mpc/application/dto/RoundResult.java
+++
@@ -1,8 +0,0 @@
-package com.zkrypto.zkmpc_api.domain.mpc.application.dto;
-
-public record RoundResult(
-        String mpcId,
-        int roundNum,
-        String resultData
-) {
-}
